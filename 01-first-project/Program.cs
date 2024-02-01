ab7b977b4e4d12f41867bb2d115b98603a4559a1
sandersonrafael <sandersonrafael-35@hotmail.com>
2024-02-01 18:21:19 -0300

    Learning about app environments

--- 01-first-project/Program.cs
+++ 01-first-project/Program.cs
@@ -5,6 +5,15 @@ using Microsoft.AspNetCore.Mvc;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+var configuration = app.Configuration; // Obtain configuration from appsettings.json
+
+// Inject configuration from application into ProductRepository class, initializing the list
+ProductRepository.Init(configuration);
+
+
+
+/* Basics From Http Methods */
+
 app.MapGet("/", () => "Hello World test with command 'dotnet watch run'");
 app.MapGet("/user", () => new { Name = "Sanderson Rafael", Age = 26 }); // Anonymous object
 
@@ -28,11 +37,30 @@ app.MapGet("/get-product/{code}", ([FromRoute] string code) => $"code: {code}");
 app.MapGet("/get-product-header", (HttpRequest request) => /* request.Headers.Authorization */ /* || */ request.Headers["Authorization"][0]);
 
 
+
 /* CRUD */
 
-app.MapGet("/products", () => ProductRepository.FindAll());
-app.MapGet("/products/{code}", ([FromRoute] string code) => ProductRepository.FindByCode(code));
-app.MapPost("/products", (Product product) => ProductRepository.Create(product));
+app.MapGet("/products", () => Results.Ok(ProductRepository.FindAll()));
+app.MapGet("/products/{code}", ([FromRoute] string code) => Results.Ok(ProductRepository.FindByCode(code)));
+app.MapPost("/products", (Product product) =>
+{
+    ProductRepository.Create(product);
+    return Results.Created();
+});
 app.MapPut("/products/{code}", ([FromRoute] string code, Product product) => ProductRepository.Update(code, product));
+app.MapDelete("/products/{code}", ([FromRoute] string code) =>
+{
+    ProductRepository.Delete(code);
+    return Results.NoContent();
+});
+
+
+/* Showing configuration in a http method */
+
+// using this notation: variable["firstPath:secondPath"], we can access the items inside a object with nested attributes
+
+if (app.Environment.IsStaging()) // Example using the Environment type in a condition
+    app.MapGet("/configuration/database", (IConfiguration configuration) =>
+        Results.Ok($"Connection: {configuration["database:connection"]}, Port: {configuration["database:port"]}"));
 
 app.Run();
--- 01-first-project/Repositories/ProductRepository.cs
+++ 01-first-project/Repositories/ProductRepository.cs
@@ -4,7 +4,14 @@ namespace _01_first_project.Repositories;
 
 public static class ProductRepository
 {
-    private static readonly List<Product> Products = [];
+    private static List<Product> Products = [];
+
+    public static void Init(IConfiguration configuration)
+    {
+        // That will get the configuration with name "Products" from appsettings.json file and convert to described type
+        List<Product>? products = configuration.GetSection("Products").Get<List<Product>>();
+        Products = products ?? [];
+    }
 
     public static List<Product> FindAll() => Products;
 
@@ -12,6 +19,7 @@ public static class ProductRepository
 
     public static Product? Create(Product product)
     {
+        if (Products.Any(p => p.Code == product.Code)) return null;
         Products.Add(product);
         return product;
     }
@@ -24,5 +32,5 @@ public static class ProductRepository
         return Products[foundIndex];
     }
 
-    public static void Delete(Product product) => Products.Remove(product);
+    public static void Delete(string code) => Products.RemoveAll(p => p.Code == code);
 }
